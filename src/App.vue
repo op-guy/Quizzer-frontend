e0d768598a91745276a61730664356e13a4f58ab
op-guy <chughsaatvik@gmail.com>
2021-07-02 18:14:01 +0530

    commit1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar dark app>
+      <v-toolbar-title>
+        <h6 class="site-title">Quizzer.com</h6>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        
+      </v-container>
+    </v-main>
+    <v-footer>
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Quizzer</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  components: {},
+
+  data: () => ({
+    result: "",
+    responseAvailable: false,
+  }),
+
+  methods: {
+    fetchQuestions() {
+      this.responseAvailable = false;
+
+      fetch("http://127.0.0.1:8000/", {
+        method: "GET",
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log(response);
+            alert(
+              "Server returned " + response.status + " : " + response.statusText
+            );
+          }
+        })
+        .then((response) => {
+          (this.responseAvailable = true), (this.result = JSON.stringify(response));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.site-title {
+  font-family: "Apple Chancery", cursive;
+  font-size: 3rem;
 }
 </style>
